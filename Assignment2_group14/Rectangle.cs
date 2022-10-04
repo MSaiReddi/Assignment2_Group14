c1ead55b7abdcb0275d01e56cc36e087d77debf9
MSaiReddi <113728609+MSaiReddi@users.noreply.github.com>
2022-10-04 16:50:54 -0400

    Containing the Completed Rectangle class

---
+++ Assignment2_group14/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_group14
+{
+    internal class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetCurrentLength()
+        {
+            return length;
+        }
+
+        public int SetNewLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+        public int GetCurrentWidth()
+        {
+            return width;
+        }
+        public int SetNewWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int GetPerimeter()
+        {
+            return 2*(length+width);
+        }
+        public int GetArea()
+        {
+            return (length * width);
+        }
+
+    }
+}
